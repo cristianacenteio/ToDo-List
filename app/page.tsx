777d4b3eb0c97abbd83fa75b6cae7844cd1221c0
cristianacenteio <cpcenteio@gmail.com>
2024-11-14 20:34:51 +0000

    created and implemented "add new tasks"

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
+import { getAllTodos } from './api'
+import AddTask from './components/AddTask'
+import TodoList from './components/TodoList'
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks)
+
   return (
-    <main>
-      <h1> Hello World</h1>
-      <Link href='/users'>Users</Link>
-      <ProductCard />
+    <main className='max-w-4xl mx-auto mt-4'>
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Todo List App</h1>
+        <AddTask />
+      </div>
+      <TodoList tasks={tasks} />
     </main>
 
   )
